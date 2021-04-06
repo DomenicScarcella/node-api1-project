dd53ab9c1cc1dba50b24e8e2f9a4a39fd5eee302
Domenic Scarcella <ls41513@yahoo.com>
2021-04-05 20:22:28 -0400

    All 16 tests pass!

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.get('/api/users', (req, res) => {
             res.status(200).json(users)
         })
         .catch(err => {
-            res.status(500).json({message: err.message})
+            res.status(500).json({message: "The users information could not be retrieved"})
         })
     
 })
@@ -24,18 +24,64 @@ server.get('/api/users/:id', (req, res) => {
     User.findById(idVar)
         .then(user => {
             if(!user){
-                res.status(404).json('User not found')
+                res.status(404).json({message: "The user with the specified ID does not exist"})
             } else {
                 res.status(200).json(user)
             }
         })
         .catch(err => {
-            res.status(500).json({message: err.message})
+            res.status(500).json({message: "The user information could not be retrieved"})
         })
 })
 
-server.get('/api/users', (req, res) => {
-    res.status(200).json({message: "Howdy!"})
+server.post('/api/users', (req, res) => {
+    const newUser = req.body 
+    if (!newUser.name || !newUser.bio){
+        res.status(400).json({message: "Please provide name and bio for the user"})
+    } else {
+        User.insert(newUser)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({message: "There was an error while saving the user to the database"})
+            })
+    }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    const {id} = req.params
+    const changes = req.body
+
+    try {
+        if(!changes.name || !changes.bio){
+            res.status(400).json({message: "Please provide name and bio for the user"})
+        } else {
+            const updateUser = await User.update(id, changes)
+            if(!updateUser){
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+            } else {
+                res.status(201).json(updateUser)
+            }
+        }
+    } catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const deletedUser = await User.remove(id)
+        if(!deletedUser) {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        } else {
+            res.status(200).json(deletedUser)
+        }
+    } catch(err){
+        res.status(500).json({message: "The user could not be removed"})
+    }
+    
 })
 
 
